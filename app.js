51cd442c618c5c56c2c23242a1df161327395cc8
farrelfauzan <farrelfauzan78@gmail.com>
2022-05-31 15:13:53 +0700

    initialize backend server

---
+++ app.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require ('express');
+const app = express ();
+const path = require('path');
+const cors = require ('cors');
+const PORT = process.env.PORT || 8081;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cors());
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static('../public'));
+
+
+app.listen(PORT, () => console.log('Server running at port', PORT));
