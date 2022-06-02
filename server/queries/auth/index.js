b4694c728bb2ae53dad6012740d6b3c130429a85
andrialfariqiid <andrialfariqiid@gmail.com>
2022-06-03 00:59:19 +0700

    fix bug login, forgot password, auth after merge

--- server/queries/auth/index.js
+++ server/queries/auth/index.js
@@ -2,66 +2,75 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const db = require("../../models");
 const query = db.users;
-const { hashPassword } = require('../../utility')
+const { hashPassword } = require("../../utility");
 
-module.exports ={
-    login:  async (data) => {
-        try {
-          let logIn = await query.findOne({
-            where: { email: data.email },
-          });
-          const resultObj = {
-            success: false,
-            message: "",
+module.exports = {
+  login: async (data) => {
+    try {
+      let logIn = await query.findOne({
+        where: { email: data.email },
+      });
+      const resultObj = {
+        success: false,
+        message: "",
+      };
+      if (!logIn) {
+        return (resultObj.message = "User not found !");
+      } else {
+        let dbPassword = logIn.dataValues.password;
+        let isPasswordValid = bcrypt.compareSync(data.password, dbPassword);
+        console.log(isPasswordValid);
+        if (!isPasswordValid) {
+          return (resultObj.message = "Wrong password !");
+        } else {
+          resultObj.success = true;
+          const secret = "MySecret";
+          const payload = {
+            user_id: logIn.dataValues.user_id,
+            username: logIn.dataValues.username,
+            email: logIn.dataValues.email,
           };
-          if (!logIn) {
-            resultObj.message = "User not found !";
-          } else {
-            let dbPassword = logIn.dataValues.password;
-            let isPasswordValid = bcrypt.compare(data.password, dbPassword);
-            if (!isPasswordValid) {
-                resultObj.message = "Wrong password !";
-            } else {
-                resultObj.success = true;
-                resultObj.data = logIn.dataValues;
-                const result = {
-                    'token': jwt.sign(resultObj.data, secret)
-                }
-              return result
-            //   return generateToken(log);
-            }
-          }
-        } catch (error) {
-          Promise.reject({message: error});
+          resultObj.data = payload;
+          const result = jwt.sign(resultObj.data, secret);
+
+          return result;
         }
-    },
+      }
+    } catch (error) {
+      return Promise.reject({ message: error });
+    }
+  },
 
-    forgotPassword: async (data) => {
-        try {
-            const encryptPassword = hashPassword(data.password);
-            const time = Date.now();
-            const checkUser = await Users.findOne({
-              where: { email: data.email },
-            });
-            if (!checkUser) {
-              return Promise.reject("Email not found !");
-            } else {
-              await Users.update(
-                {
-                  password: encryptPassword,
-                  updatedAt: time,
-                },
-                { where: { email: checkUser.dataValues.email } }
-              );
-            }
-          } catch (error) {
-            return Promise.reject({ message: error });
-          }
-    },
+  forgotPassword: async (data) => {
+    try {
+      const encryptPassword = hashPassword(data.password);
+      const time = Date.now();
+      const checkUser = await query.findOne({
+        where: { email: data.email },
+      });
+      if (!checkUser) {
+        return Promise.reject("Email not found !");
+      } else {
+        if (data.password === "") {
+          return Promise.reject("Password must be filled !");
+        } else {
+          await query.update(
+            {
+              password: encryptPassword,
+              updatedAt: time,
+            },
+            { where: { email: checkUser.dataValues.email } }
+          );
+        }
+      }
+    } catch (error) {
+      return Promise.reject({ message: error });
+    }
+  },
 
-    authenticateToken: (token) => {
-        const secret = "MySecret";
-        let data = jwt.verify(token, secret);
-        return data;
-    },
-}
+  authenticateToken: (token) => {
+    const secret = "MySecret";
+    let data = jwt.verify(token, secret);
+    return data;
+  },
+};
