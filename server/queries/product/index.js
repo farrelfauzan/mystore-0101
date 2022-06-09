f7a5698d559b5e4aa4eec3b12125f52bf822f698
andrialfariqiid <andrialfariqiid@gmail.com>
2022-06-09 22:03:20 +0700

    delete try catch in queries

--- server/queries/product/index.js
+++ server/queries/product/index.js
@@ -3,21 +3,17 @@ const queryProduct = db.products;
 
 module.exports = {
   getAllProducts: async () => {
-    try {
-      const productAttributes = [
-        "product_id",
-        "name",
-        "imageUrl",
-        "category",
-        "description",
-        "price",
-      ];
-      const data = await queryProduct.findAll({
-        attributes: productAttributes,
-      });
-      return data;
-    } catch (error) {
-      return Promise.reject(error);
-    }
+    const productAttributes = [
+      "product_id",
+      "name",
+      "imageUrl",
+      "category",
+      "description",
+      "price",
+    ];
+    const data = await queryProduct.findAll({
+      attributes: productAttributes,
+    });
+    return data;
   },
 };
